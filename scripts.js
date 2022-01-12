3fd08e25a6bf9a927c6d300ac2cca98579da5efb
Erik-EFL <erik.lima.f@outlook.com>
2022-01-12 03:36:18 -0300

    Adicionando requisitos 3 e 20:rocket:

--- scripts.js
+++ scripts.js
@@ -1,5 +1,21 @@
 const loginBtn = document.querySelector('.btn-login');
+const textArea = document.querySelector('#textarea');
+
+textArea.addEventListener('keypress', (e) => {
+  const maximo = 500;
+  if (textArea.value.length >= maximo) {
+    e.preventDefault();
+  }
+});
+
+textArea.addEventListener('keyup', () => {
+  const restante = document.querySelector('#remaining');
+  restante.innerHTML = 500 - textArea.value.length;
+});
 
 loginBtn.addEventListener('click', () => {
-
+  const email = document.querySelector('#email');
+  const password = document.querySelector('#password');
+  return email.value !== 'tryber@teste.com' && password.value !== '123456'
+    ? alert('Email ou senha inválidos.') : alert('Olá, Tryber!');
 });
